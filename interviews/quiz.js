efaf451572b9fdc79152715f862d0b57f352b6a2
LOCO185 <jloco151@gmail.com>
2022-09-25 21:54:23 +0300

    commit

--- interviews/quiz.js
+++ interviews/quiz.js
@@ -1,12 +1,10 @@
-// var a = "10";
-// var b = 0.11;
-// console.log(a+b);
-// Answer: 100.11
+var a = "10";
+var b = 0.11;
+console.log(a + b);
+// Answer: 100.11;
 
-// A = 10;
-// B = "10"
-// C = (A == B)
-// console.log(C);
+A = 10;
+B = "10";
+C = A == B;
+console.log(C);
 // Answer: true
-
-
